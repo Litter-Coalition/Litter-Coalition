8cd39590e99b47da1cd62cd180bebda6baf49ad4
AnansiOmega <ignasbutautas7@gmail.com>
2021-03-13 13:35:38 -0600

    added auto provider in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
+const domain = process.env.DOMAIN
+const clientId = process.env.CLIENTID
+const redirectUri = process.env.REDIRECTURI
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={redirectUri}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
