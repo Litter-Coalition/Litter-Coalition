d6cf0e003dff91885a255112bc0551a5e47e3544
Dustin Wilson <dustinwilson@Dustins-MBP.lan>
2021-03-13 08:56:17 -0500

    update to dataproc + docs

--- README.md
+++ README.md
@@ -15,12 +15,13 @@ Sample data available in Postgres and as MBtiles via the following:
 
 1. Download NYC [streets data](https://data.cityofnewyork.us/City-Government/NYC-Street-Centerline-CSCL-/exjm-f27b) from NYC Open Data Portal in the `Original` format and save to `./data/raw/` (this file is ~70MB and the folder is included in `.gitignore`)
 
-2. Exec into `litter-coalistion_db_1` with `docker exec -ti litter-coalition_db_1 bash` and run `./bash dataproc_geo.sh` to populate the DB and generate mbtiles.
+2. Exec into `litter-coalition_db_1` with `docker exec -ti litter-coalition_db_1 bash` and run `bash ./dataproc.sh ./data/raw/Centerline.zip centerline` to populate the DB and generate mbtiles.
 
    - Available in DB `postgis` @ `public.centerline`
-   - Available as MBtiles on Zoom (10,18) at `/data/layers/centerline`
+   - Available as MBtiles on Zoom (10,18) at `/data/layers/centerline_tiles`
+   - Available as GeoJSON @ `/data/layers/centerline.geojson`
 
-This pattern should hold for (most) geospatial available through the open data portal, `sed "s/centerline/<NEW TERM>/g" dataproc_geo.sh` should allow for drop in replacement of any other dataset.
+This pattern should hold for (most) geospatial available through the open data portal, `bash dataproc.sh <LOCAL FILE> <LAYERNAME>` should allow for drop in replacement of any other dataset.
 
 ### Flask
 
--- geo_dataproc/dataproc.sh
+++ geo_dataproc/dataproc.sh
@@ -1,32 +1,41 @@
 #! bin/sh
 
+if [ ! -f "$1" ]; then
+    exit
+fi
+
 # Unzip Static Geospatial Data
 mkdir -p ./data/layers &&\
-    unzip -o ./data/raw/Centerline.zip -d ./data/layers/centerline
+    unzip -o $1 -d ./data/layers/$2
 
 # Write Streets as DB Objects -> PostGIS; Convert to EPSG 4326, (lng, lat)
 # Option: Serving Dynamic Tiles
 ogr2ogr -overwrite \
     -f "PostgreSQL" PG:"dbname=${POSTGRES_DB} user=${POSTGRES_USER}" \
-    ./data/layers/centerline \
-    -nln centerline \
+    ./data/layers/$2 \
+    -nln $2 \
     -lco OVERWRITE=yes \
     -t_srs "EPSG:4326"
 
 # Write Original Shapefiles To GeoJSON; Convert to EPSG 4326
 # Option: Serving Static Tiles
-ogr2ogr -f GeoJSON \
+ogr2ogr -overwrite\
+    -f GeoJSON \
     -t_srs "EPSG:4326" \
-    ./data/layers/centerline.geojson  ./data/layers/centerline
+    ./data/layers/$2.geojson  ./data/layers/$2/
 
 # To MBTiles - Create a SQLiteDB w. all shapes prepped for /x/y/z extraction
 # Use level 10->18
 # Ref: https://fuzzytolerance.info/blog/2017/02/02/Making-your-own-tiles-with-Tippecanoe/
 # Ref: https://wiki.openstreetmap.org/wiki/Zoom_levels
-tippecanoe -L centerline:./data/layers/centerlines.geojson \
+tippecanoe -L centerline:./data/layers/$2.geojson \
     --drop-densest-as-needed \
-    -o ./data/layers/centerline.mbtiles -z18 -Z10
+    -o ./data/layers/$2.mbtiles -z18 -Z10 \
+    --force
 
 # MB-Util; Extract all Tiles...
 ./mbutil/mb-util /data/layers/centerline.mbtiles \
-    /data/layers/centerline
+    /data/layers/$2_tiles
+
+# Cleanup, save a bit of space
+rm -rf /data/layers/$2
