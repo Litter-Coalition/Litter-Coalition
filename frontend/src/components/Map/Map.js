579542183578238f3957025da4d82ccf759659ad
kevin <krecoding@gmail.com>
2021-03-14 14:19:35 -0400

    protoyping change route name feat

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -8,80 +8,59 @@ import AddLocationMarker from "./AddLocationMarker";
 // import CurrentLocation from "./CurrentLocation"; // Current Location Marker creates a marker on the user's current location
 import Polygons from "./Polygons";
 import MapUI from "./MapUI";
-import { Col, Form, FormGroup, Label, Row, Input } from "reactstrap";
 
 const Map = (props) => {
-	const [coords, setCoords] = useState([[40.75, -73.931]]);
-	const [polygons, setPolygons] = useState([
-		// {
-		// 	popup: "Proud Astorian",
-		// 	shape: [
-		// 		[40.7588954203221, -73.9190196990967],
-		// 		[40.761300880922235, -73.91726016998292],
-		// 		[40.760098161503265, -73.91472816467287],
-		// 		[40.75575843890192, -73.91219615936281],
-		// 	],
-		// },
-	]);
+  const [coords, setCoords] = useState([[40.75, -73.931]]);
+  const [polygons, setPolygons] = useState([
+    {
+      popup: "Proud Astorian",
+      shape: [
+        [40.7588954203221, -73.9190196990967],
+        [40.761300880922235, -73.91726016998292],
+        [40.760098161503265, -73.91472816467287],
+        [40.75575843890192, -73.91219615936281],
+      ],
+    },
+    {
+      popup: "Anti-Litter Group",
+      shape: [
+        [40.76120336394223, -73.92378330230714],
+        [40.76614404424928, -73.91974925994874],
+      ],
+    },
+    {
+      popup: "Hell's Kitchen Team",
+      shape: [
+        [40.772221877329024, -73.92595052719118],
+        [40.774171866400664, -73.93185138702394],
+        [40.77228687788679, -73.9329242706299],
+        [40.77205937565639, -73.93230199813844],
+      ],
+    },
+  ]);
 
-	const addNewPolygon = (polygon) => {
-		setPolygons((polygons) => [...polygons, polygon]);
-	};
+  const addNewPolygon = (polygon) => {
+    setPolygons((polygons) => [...polygons, polygon]);
+  };
 
-	const updateCrds = () => {
-		GeoLocator.getCoords();
-	};
+  const updateCrds = () => {
+    console.log(GeoLocator.getCoords());
+  };
 
-	const routes = polygons.map((polygon, index) => {
-		return (
-			<Row form>
-				<Col md={6}>
-					<FormGroup>
-						<Label for="exampleState">{polygon.popup}</Label>
-						<Input
-							type="email"
-							name="email"
-							id="exampleEmail"
-							placeholder="Rename Route"
-						/>
-					</FormGroup>
-				</Col>
-			</Row>
-		);
-	});
-
-	// <Row form>
-	// 				<Col md={6}>
-	// 					<FormGroup>
-	// 						<Input
-	// 							type="email"
-	// 							name="email"
-	// 							id="exampleEmail"
-	// 							placeholder="with a placeholder"
-	// 						/>
-	// 					</FormGroup>
-	// 				</Col>
-	// 			</Row>
-
-	return (
-		<div>
-			<button onClick={updateCrds}>Locate Me</button>
-			<MapContainer
-				center={[40.75, -73.931]}
-				zoom={12}
-				scrollWheelZoom={true}
-			>
-				<MapUI addNewPolygon={addNewPolygon} />
-				<TileLayer
-					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-					//   url="http://localhost:8080/data/centerline/{z}/{x}/{y}.pbf"
-				/>
-				<Polygons polygons={polygons} />
-			</MapContainer>
-			<Form>{routes}</Form>
-		</div>
-	);
+  return (
+    <div>
+      <button onClick={updateCrds}>Locate Me</button>
+      <MapContainer center={[40.75, -73.931]} zoom={12} scrollWheelZoom={true}>
+        <MapUI addNewPolygon={addNewPolygon} />
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="http://localhost:8080/data/centerline/{z}/{x}/{y}.pbf"
+        />
+        <Polygons polygons={polygons} />
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
--- frontend/src/components/Map/Polygons.js
+++ frontend/src/components/Map/Polygons.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Polyline, Popup } from "react-leaflet";
+import { Marker, Polyline, Popup, Tooltip } from "react-leaflet";
+import { Input } from "reactstrap";
 
 const Polygons = ({ polygons }) => {
 	const mapped_polygons = polygons.map((polygon, index) => {
+		console.log(index, polygons[index + 1], polygons)
+		console.log(polygons[index + 1] == null ? true : false)
 		return (
 			<Polyline key={index} positions={[polygon.shape]}>
-				<Popup>{polygon.popup}</Popup>
+					<Tooltip position={polygon.shape[0]} permanent={polygons[index + 1] == null ? true : false}>
+						<Input placeholder={polygon.popup}></Input>
+					</Tooltip>
 			</Polyline>
 		);
 	});
