8b7da1308af9f95ee8e91c8788877736a8a44486
kre <38985876+kre64@users.noreply.github.com>
2021-03-13 21:01:24 -0500

    Kevins features (#8)
    * testing
    
    * utils class for geolocation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
 import "./App.css";
-
 import { Switch, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ViewEventsPage from "./pages/ViewEventsPage";
 import ReportPage from "./pages/ReportPage";
 import FAQPage from "./pages/FAQPage";
 
-// import Map from "./components/Map/Map";
-// import Map2 from "./components/Map2";
-
 function App() {
   return (
     <div className="App">
+      <button onClick={() => GeoLocator.getCoords()}></button>
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/view-events" component={ViewEventsPage} />
