5856cd1e5d63128c48628b8c4bfca73652751173
AnansiOmega <ignasbutautas7@gmail.com>
2021-03-14 11:59:05 -0500

    working on a re-usable share button for a user to post to social media (#11)

---
+++ frontend/src/components/ShareButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ShareButton = ({platform, hashtags}) => {
+
+    return(
+        <>
+            { platform === 'facebook' ?
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=https://github.com/AnansiOmega&=TrashWarriors"
+                    onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;"
+                    target="_blank">Share</a>
+                :
+                <a  className="twitter-share-button"
+                    href="https://twitter.com/intent/tweet"
+                    data-size="large"
+                    data-text="Cleaning up New York City one bag a time"
+                    data-url="https://github.com/AnansiOmega"
+                    data-hashtags={hashtags?.join(',')}
+                    data-related="twitterapi,twitter">
+                    Tweet
+                </a>
+
+            }
+        </>
+    )
+}
+
+export default ShareButton
