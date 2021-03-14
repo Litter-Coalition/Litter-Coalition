8165bb512559040a9c85db4f0858e900d9657a0f
kre <krecoding@gmail.com>
2021-03-13 23:59:07 -0500

    Updating polygons arr with new lines on every draw

--- frontend/src/components/Map/MapUI.js
+++ frontend/src/components/Map/MapUI.js
@@ -3,30 +3,54 @@ import { Button } from "reactstrap";
 import { useMapEvents } from "react-leaflet";
 import { useLeafletContext } from "@react-leaflet/core";
 import "./MapUI.css";
+import { polygon } from "leaflet";
 
-const MapUI = (props) => {
+const MapUI = ({ addNewPolygon }) => {
 	const context = useLeafletContext();
-    const [drawing, setDrawing] = useState(false);
+	const [drawing, setDrawing] = useState(false);
+	const [polyArr, setPolyArr] = useState([]);
 
 	useMapEvents({
 		click(e) {
-			console.log(e.latlng);
+			if (drawing) {
+                const polyArrCopy = polyArr;
+                polyArrCopy.push([e.latlng.lat, e.latlng.lng])
+                console.log(polyArr)
+                setPolyArr(polyArrCopy)
+                addNewPolygon({
+                    popup: "Hell's Kitchen Team",
+                    shape: polyArrCopy
+                });
+				// setPolyArr((polyArr) => [...polyArr, [e.latlng.lat, e.latlng.lng]]);
+			}
 		},
 	});
 	console.log(context);
 
-    const draw = () => {
-        if (drawing) {
-            
-        } else {
-            setDrawing(true);
-        }
-    }
+	const draw = (e) => {
+		setDrawing(true);
+	};
+
+	const stopDrawing = () => {
+		addNewPolygon({
+			popup: "Hell's Kitchen Team",
+			shape: polyArr
+		});
+
+        setPolyArr([])
+	};
+
+    console.log(polyArr)
 
 	return (
 		<div>
-            aaaaaaa
-			<Button className="test-btn" onClick={draw}>aaaaaaaaa</Button>
+			aaaaaaa
+			<Button className="test-btn" onClick={draw}>
+				draw
+			</Button>
+			<Button className="test-btn" onClick={stopDrawing}>
+				stop draw
+			</Button>
 		</div>
 	);
 };
