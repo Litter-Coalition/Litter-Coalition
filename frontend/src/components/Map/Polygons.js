50ab7ef418f58d6191dc3cd55d9aeb5691467f2a
kevin <krecoding@gmail.com>
2021-03-14 14:41:26 -0400

    editable popups

--- frontend/src/components/Map/Polygons.js
+++ frontend/src/components/Map/Polygons.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Marker, Polyline, Popup, Tooltip } from "react-leaflet";
+import { Marker, Polyline, Tooltip } from "react-leaflet";
+import Popup from 'react-leaflet-editable-popup';
 import { Input } from "reactstrap";
 
 const Polygons = ({ polygons }) => {
 	const mapped_polygons = polygons.map((polygon, index) => {
-		console.log(index, polygons[index + 1], polygons)
-		console.log(polygons[index + 1] == null ? true : false)
+		console.log(index, polygons[index + 1], polygons);
+		console.log(polygons[index + 1] == null ? true : false);
 		return (
 			<Polyline key={index} positions={[polygon.shape]}>
-					<Tooltip position={polygon.shape[0]} permanent={polygons[index + 1] == null ? true : false}>
+				<Popup
+					position={polygon.shape[0]}
+					open={polygons[index + 1] == null ? true : false}
+				>
+					<div>
 						<Input placeholder={polygon.popup}></Input>
-					</Tooltip>
+					</div>
+				</Popup>
 			</Polyline>
 		);
 	});
