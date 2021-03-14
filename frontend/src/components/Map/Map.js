87b429e1e8679d5573879e76e47b3c8e670fea7a
kndshein <75239264+kndshein@users.noreply.github.com>
2021-03-13 21:54:06 -0500

    Kaung map (#10)
    * Added functionality to draw shapes and push
    
    * Added Popups to Shapes
    
    * Switched Polygon to Polyline. Fixed bug with new Event
    
    * Renamed 'Add an Event' to 'Add a Route'

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -9,33 +9,62 @@ import Polygons from "./Polygons";
 
 const Map = (props) => {
   const [polygons] = React.useState([
-    [
-      [40.7588954203221, -73.9190196990967],
-      [40.761300880922235, -73.91726016998292],
-      [40.760098161503265, -73.91472816467287],
-      [40.75575843890192, -73.91219615936281],
-    ],
-    [
-      [40.76120336394223, -73.92378330230714],
-      [40.76614404424928, -73.91974925994874],
-    ],
-    [
-      [40.772221877329024, -73.92595052719118],
-      [40.774171866400664, -73.93185138702394],
-      [40.77228687788679, -73.9329242706299],
-      [40.77205937565639, -73.93230199813844],
-    ],
+    {
+      popup: "Proud Astorian",
+      shape: [
+        [40.7588954203221, -73.9190196990967],
+        [40.761300880922235, -73.91726016998292],
+        [40.760098161503265, -73.91472816467287],
+        [40.75575843890192, -73.91219615936281],
+      ],
+    },
+    {
+      popup: "Anti-Litter Group",
+      shape: [
+        [40.76120336394223, -73.92378330230714],
+        [40.76614404424928, -73.91974925994874],
+      ],
+    },
+    {
+      popup: "Hell's Kitchen Team",
+      shape: [
+        [40.772221877329024, -73.92595052719118],
+        [40.774171866400664, -73.93185138702394],
+        [40.77228687788679, -73.9329242706299],
+        [40.77205937565639, -73.93230199813844],
+      ],
+    },
   ]);
+
+  const [addNewEvent, setAddNewEvent] = React.useState(false);
+  // const [newEventForm, setNewFormEvent] =
+  const [newEvent, setNewEvent] = React.useState([]);
+
+  const handleAdd = () => {
+    setAddNewEvent(true);
+  };
+
+  const handleRemove = () => {
+    setAddNewEvent(false);
+    polygons.push({ popup: "test", shape: newEvent });
+    setNewEvent([]);
+  };
 
   return (
     <div>
+      {addNewEvent ? (
+        <button onClick={() => handleRemove()}>Finish Drawing</button>
+      ) : (
+        <button onClick={() => handleAdd()}>Add a Route</button>
+      )}
       <MapContainer center={[40.75, -73.931]} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <AddLocationMarker />
-        <Polygons polygons={polygons} />
+        {addNewEvent ? <AddLocationMarker newEvent={newEvent} /> : null}
+        {addNewEvent ? <Polygons polygons={newEvent} /> : null}
+        <Polygons polygons={polygons} newEvent={newEvent} />
       </MapContainer>
     </div>
   );
